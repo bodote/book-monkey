4931ae58731c3d6a322ca36abc530776493278ca
Bodo Teichmann <bodo.teichmann@brandad-systems.de>
2022-08-29 21:59:07 -0700

    added e2d test; fixed github pages problem

--- cypress/e2e/book-edit.cy.ts
+++ cypress/e2e/book-edit.cy.ts
@@ -1,19 +1,14 @@
 describe('edit page', () => {
   it('should save the changed author', () => {
-    let count = 0;
-    cy.on('window:confirm', (str) => {
-      count += 1;
-      switch (count) {
-        case 1:
-          expect(str).contains('Really want to be');
-          return true;
-        case 2:
-          throw new Error('confirm window should only happen once');
-          // reject the confirmation
-          return false;
+    cy.visit('/admin/edit/9783864907791', {
+      onBeforeLoad: (window) => {
+        cy.stub(window, 'confirm').as('confirm').returns(true);
       }
     });
-    cy.visit('/admin/edit/9783864907791');
     cy.contains('h2', 'Edit existing Book:');
+    cy.get('form').find('#title');
+    cy.get('@confirm')
+      .should('have.been.calledOnce')
+      .and('be.calledWith', 'Really want to be Admin?');
   });
 });
