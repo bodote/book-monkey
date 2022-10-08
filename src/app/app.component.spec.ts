47162dda196a538384a336dc7ade939283c4448c
Bodo Teichmann <bodo.teichmann@brandad-systems.de>
2022-10-08 08:10:12 +0200

    stryker error if run only for "mutate": ["src/app/app.component.ts",...

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,51 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { AppState, loaderFeatureKey } from './store';
+import { mockState } from './store/index.spec';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let store: MockStore;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent]
+      declarations: [AppComponent],
+      providers: [
+        provideMockStore<AppState>({
+          initialState: mockState()
+        })
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    store = TestBed.inject(MockStore);
   });
 
-  xit('should create the app', () => {
+  it('should create the app without loading spinner', () => {
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+    const svgSpinner = fixture.debugElement.query(
+      By.css('[data-id="loadingSpinner"]')
+    );
+    expect(svgSpinner).toBeFalsy();
+  });
+  it('should show loading spinner', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+    store.setState(
+      mockState({
+        [loaderFeatureKey]: { loading: true }
+      })
+    );
+    fixture.detectChanges();
+    const svgSpinner = fixture.debugElement.query(
+      By.css('[data-id="loadingSpinner"]')
+    );
+    expect(svgSpinner).toBeTruthy();
   });
 });
